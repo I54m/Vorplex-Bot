3f818bd788b687ad0e7e61b3591114ce5f5e2433
I54m <54mpenguin@gmail.com>
2021-03-27 21:22:55 +1300

    Add fixroles command for making sure everyone in the guild has the joinrole

---
+++ commands/fixroles.js
@@ -0,0 +1,24 @@
+const botconfig = require("./../botconfig.json");
+const Discord = require("discord.js");
+
+let inprogress = false;
+
+module.exports.run = async (bot, message, args) => {
+    if (inprogress) return message.reply("Already processing the previous fixroles, this could take up to 30 minutes. Please try again later!")
+    if (!botconfig.joinrole) return message.reply("Unable to find join role in config!");
+    let joinrole = member.guild.roles.find(`name`, botconfig.joinrole);
+    if (!joinrole) return message.reply("Unable to find join role!");
+    async () => {
+        message.guild.members.cache.forEach(member => member.roles.add(joinrole));
+        inprogress = false;
+    };
+    message.reply("Processing now, please allow up to 30 minutes to process this command!");
+    inprogress = true;
+}
+
+module.exports.help = {
+  name: "fixroles",
+  permission: "ADMINISTRATOR",
+  usage: "",
+  description: "Give everyone the member role if the welcome role is not functioning."
+}
