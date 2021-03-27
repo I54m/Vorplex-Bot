c99239ac6c13e05f619f1bba1ae0803671a6bdb8
I54m <54mpenguin@gmail.com>
2021-03-27 21:36:41 +1300

    Changed all statuses to listening

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot) => {
     let guild = bot.guilds.get(`${botconfig.guildid}`);
     setInterval(function () {
         let status = statuses[Math.floor(Math.random() * statuses.length)]
-        bot.user.setActivity(`${status}`, { type: "PLAYING" });
+        bot.user.setActivity(`${status}`, { type: "LISTENING" });
     }, 10000);
     /*
     Object.keys(advertisers).forEach((key, index, array) =>{
