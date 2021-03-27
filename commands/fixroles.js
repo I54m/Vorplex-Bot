39fd002082fa2396df19b54e1984a848e7ed88e2
I54m <54mpenguin@gmail.com>
2021-03-27 21:36:55 +1300

    Fixed error with fix roles command

--- commands/fixroles.js
+++ commands/fixroles.js
@@ -6,7 +6,7 @@ let inprogress = false;
 module.exports.run = async (bot, message, args) => {
     if (inprogress) return message.reply("Already processing the previous fixroles, this could take up to 30 minutes. Please try again later!")
     if (!botconfig.joinrole) return message.reply("Unable to find join role in config!");
-    let joinrole = member.guild.roles.find(`name`, botconfig.joinrole);
+    let joinrole = message.guild.roles.find(`name`, botconfig.joinrole);
     if (!joinrole) return message.reply("Unable to find join role!");
     async () => {
         message.guild.members.cache.forEach(member => member.roles.add(joinrole));
