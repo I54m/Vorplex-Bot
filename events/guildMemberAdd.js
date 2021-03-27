a736334c37f582e9c5f35fa16e7d2f6a7c688801
I54m <54mpenguin@gmail.com>
2021-03-27 21:01:35 +1300

    Changed Role to be given first regardless of errors in the welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,30 +3,30 @@ const botconfig = require("./../botconfig.json");
 const messagelist = require("./../messages.json");
 
 module.exports.run = async (bot, member) => {
-  let welcomechannel = member.guild.channels.find(`name`, botconfig.welcomechannel);
-    if (!welcomechannel) return console.log("Unable to find welcome channel!");
-  let embedColour = botconfig.embedColour;
-  let messageArray = messagelist.welcomemessages;
-  let messageNumber = Math.floor(Math.random() * messageArray.length);
-  let message = messageArray[messageNumber];
-  if (message.includes("{number}")){
-    let number = Math.floor(Math.random() * 3678);
-    message = message.replace(`{number}`, `${number}`);
-  }
-  let welcomeEmbed = new Discord.RichEmbed()
-  .setTitle(`**Welcome ${member.displayName} to the Vorplex Server Discord!**`)
-  .setColor(`${embedColour}`)
-  .setDescription(message.replace(`{user}`, `${member.user.tag}`))
-  .addField("Rules & Info", `${member.guild.channels.find(`name`, botconfig.rulesinfochannel).toString()}`, true)
-  .setThumbnail("https://api.i54m.com/Vorplex/Vorplex-Server-Icon-x1000.png")
-  .setFooter(`Total: ${member.guild.memberCount} members`);
-  welcomechannel.send(welcomeEmbed);
     if (!botconfig.joinrole) return console.log("Unable to find join role in config!");
-  let joinrole = member.guild.roles.find(`name`, botconfig.joinrole);
-  if (!joinrole) return console.log("Unable to find join role!");
-  await(member.addRole(joinrole.id));
+    let joinrole = member.guild.roles.find(`name`, botconfig.joinrole);
+    if (!joinrole) return console.log("Unable to find join role!");
+    await (member.addRole(joinrole.id));
+    let welcomechannel = member.guild.channels.find(`name`, botconfig.welcomechannel);
+    if (!welcomechannel) return console.log("Unable to find welcome channel!");
+    let embedColour = botconfig.embedColour;
+    let messageArray = messagelist.welcomemessages;
+    let messageNumber = Math.floor(Math.random() * messageArray.length);
+    let message = messageArray[messageNumber];
+    if (message.includes("{number}")) {
+        let number = Math.floor(Math.random() * 3678);
+        message = message.replace(`{number}`, `${number}`);
+    }
+    let welcomeEmbed = new Discord.RichEmbed()
+        .setTitle(`**Welcome ${member.displayName} to the Vorplex Server Discord!**`)
+        .setColor(`${embedColour}`)
+        .setDescription(message.replace(`{user}`, `${member.user.tag}`))
+        .addField("Rules & Info", `${member.guild.channels.find(`name`, botconfig.rulesinfochannel).toString()}`, true)
+        .setThumbnail("https://api.i54m.com/Vorplex/Vorplex-Server-Icon-x1000.png")
+        .setFooter(`Total: ${member.guild.memberCount} members`);
+    welcomechannel.send(welcomeEmbed);
 }
 
 module.exports.help = {
-  name: "guildMemberAdd"
+    name: "guildMemberAdd"
 }
