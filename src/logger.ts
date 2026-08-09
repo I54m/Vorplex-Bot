41583478101f80d7ea8c1d45901d2bce77549934
I54m <i54m@i54m.com>
2026-08-09 21:31:02 +1200

    Run prettier to reformat

--- src/logger.ts
+++ src/logger.ts
@@ -1,24 +1,29 @@
-import { createLogger, format, transports } from "winston";
+import { createLogger, format, transports } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 
-import type { Logger } from "winston";
+import type { Logger } from 'winston';
 
 const customFormat = format.printf(({ timestamp, level, message }) => {
     return `[${timestamp} ${level.toLocaleUpperCase()}] ${message}`;
 });
 
 const customFormatColoured = format.printf(({ timestamp, level, message }) => {
-    return format.colorize().colorize(level, `[${timestamp} ${level.toLocaleUpperCase()}] ${message}`);
+    return format
+        .colorize()
+        .colorize(
+            level,
+            `[${timestamp} ${level.toLocaleUpperCase()}] ${message}`,
+        );
 });
 
 const logger: Logger = createLogger({
     level: 'info',
     format: format.combine(
         format.timestamp({
-        format: 'DD-MMM-YYYY HH:mm:ss'
+            format: 'DD-MMM-YYYY HH:mm:ss',
         }),
         format.errors({ stack: true }),
-        customFormat
+        customFormat,
     ),
     transports: [
         new transports.Console({
@@ -29,21 +34,25 @@ const logger: Logger = createLogger({
 
 // If we're in production then log to the files
 if (process.env.NODE_ENV === 'production') {
-    logger.add(new DailyRotateFile({
-        level: 'error',
-        filename: 'logs/%DATE%/error.log',
-        datePattern: 'YYYY-MM-DD',
-        zippedArchive: true,
-        maxSize: '20m',
-        maxFiles: '14d'
-    }))
-    logger.add(new DailyRotateFile({
-        filename: 'logs/%DATE%/combined.log',
-        datePattern: 'YYYY-MM-DD',
-        zippedArchive: true,
-        maxSize: '20m',
-        maxFiles: '14d'
-    }));
+    logger.add(
+        new DailyRotateFile({
+            level: 'error',
+            filename: 'logs/%DATE%/error.log',
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d',
+        }),
+    );
+    logger.add(
+        new DailyRotateFile({
+            filename: 'logs/%DATE%/combined.log',
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d',
+        }),
+    );
 }
 
 export default logger;
